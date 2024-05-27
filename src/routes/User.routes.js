0c533880dfb006ec456942e8bdd2574ec06bdc60
Harshit <richhariyaharshit684@gmail.com>
2024-05-27 17:26:00 +0530

    update router

--- src/routes/User.routes.js
+++ src/routes/User.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changecurruntuserpassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWachHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registeruser,
+  updateAccountDetails,
+  updateCoverImage,
+  updateUserAvtar,
 } from "../controllers/User.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyjwt } from "../middlewares/auth.middleware.js";
@@ -28,5 +35,23 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyjwt, logoutUser);
 
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyjwt, changecurruntuserpassword);
+
+router.route("/current-user").get(verifyjwt, getCurrentUser);
+
+router.route("update-account").patch(verifyjwt, updateAccountDetails);
+
+router
+  .route("/avtar")
+  .patch(verifyjwt, upload.single("avatar"), updateUserAvtar);
+
+router
+  .route("/cover-image")
+  .patch(verifyjwt, upload.single("/coverImage"), updateCoverImage);
+
+router.route("/c/:username").get(verifyjwt, getUserChannelProfile);
+
+router.route("/history").get(verifyjwt, getWachHistory);
 
 export default router;
